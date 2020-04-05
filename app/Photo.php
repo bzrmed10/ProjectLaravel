4bfbcad892aca4185b61f10c17adcb816df3473b
= <=>
2020-04-05 23:30:39 +0200

    edit user interface

--- app/Photo.php
+++ app/Photo.php
@@ -6,8 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
+
+    protected $directory = '/images/'; 
     protected $fillable = [
         'file'
     ];
 
+
+    public function getFileAttribute($photo){
+        
+        return $this->directory . $photo;
+    
+    }
 }
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+
+@section('content')
+
+<h1>Edit User</h1>
+
+<div class="col-sm-3">
+    <img class="img-responsive img-rounded" src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}" alt="">
+</div>
+<div class="col-sm-9">
+    {!! Form::model($user,['methode'=>'POST','action'=>['AdminUsersController@update',$user->id],'files'=>true]) !!}
+    <div class="form-group">
+        {!!Form::label('name','Name :')!!}
+        {!!Form::text('name',null ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::label('email','Email :')!!}
+        {!!Form::email('email',null ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::label('role_id','Role :')!!}
+        {!!Form::select('role_id', $roles ,null ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::label('is_active','Status :')!!}
+        {!!Form::select('is_active', array(1 => 'Active', 0  => 'Not active' ) , null ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::label('photo_id','Picture :')!!}
+        {!!Form::file('photo_id'  ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::label('password','Password :')!!}
+        {!!Form::password('password' ,['class' => 'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!Form::submit('Creat User' ,['class' => 'btn btn-primary'])!!}
+
+    </div>
+    {!! Form::close() !!}
+</div>
+
+
+
+   
+
+    @include('includes.form-err')
+@endsection
